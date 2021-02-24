a0d34162ccc310396c7b9d2534f14d38dbb8b338
Mrinalpm <mrinalpm@hotmail.com>
2021-02-24 17:12:23 -0600

    Added feature for filtering movies

--- app/src/main/java/com/example/ixalan/presentation/MovieListActivity.java
+++ app/src/main/java/com/example/ixalan/presentation/MovieListActivity.java
@@ -14,89 +14,89 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.ixalan.R;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class MovieListActivity extends AppCompatActivity
 
 {
-    //Array of movie names, temporary until objects are implemented
-    String[] movie_names = new String[]{"poster_1", "poster_2", "poster_3", "poster_4"};
+    //List to store currently running and upcoming movies
+    //TODO: Once objects implemented, change string to object
+    private List<String> list_of_movies;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.movie_list);
 
-        LinearLayout layout = (LinearLayout)findViewById(R.id.movies_display_layout);
-        for (int i = 0; i < movie_names.length; i++)
-        {
-            Button btn = new Button(this);
-            btn.setText(movie_names[i]);
-            layout.addView(btn);
-        }
+        //Initialize action event for the movie filter
+        initializeMoviesFilterActionEvents();
+
+        //TODO: Initialize list of currently running and upcoming movies from db through logic layer and store in list_of_movies.
+        //Currently just use strings as objects haven't been implemented yet
+        list_of_movies = new ArrayList<String>(){{
+            add("Movie 1");
+            add("Movie 2");
+            add("Movie 3");
+            add("Movie 4");
+        }};
+
+        populateMovies();
     }
 
     /*
-       Function to populate list of movies
-       Parameters: Array of movies
+    Action event when a filter is applied
+    */
+    private void initializeMoviesFilterActionEvents()
+    {
+        Spinner spinner = (Spinner)findViewById(R.id.movies_filter);
+        spinner.setOnItemSelectedListener(
+                new AdapterView.OnItemSelectedListener() {
+                    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+
+                        String filter_criteria = parent.getItemAtPosition(pos).toString(); //filter that was selected
+                        String[] array = getResources().getStringArray(R.array.movie_display_filters); //list of acceptable filters
+
+                        if (Arrays.asList(array).contains(filter_criteria))
+                        { //ensure valid filter criteria is selected
+
+                            //TODO: Get list of movies based on filter from db through logic layer and store in list_of_movies.
+
+                            //Once we know what movies meet filter criteria, populate them in-app
+                            populateMovies();
+                        }
+                    }
+                    public void onNothingSelected(AdapterView<?> parent) {
+                    }
+                });
+    }
+
+    /*
+       To display list of movies in app
+       Parameters: None
        Return: None
        TODO: Once objects are implemented, change code to consider objects instead of generic strings
     */
-    private void populateMovies(Object[] movies)
+    private void populateMovies()
     {
         //Clear layout of all entries
         LinearLayout layout = (LinearLayout)findViewById(R.id.movies_display_layout);
         layout.removeAllViews();
 
         //Add entries one by one
-        for (int i = 0; i < movies.length; i++)
+        if (list_of_movies != null)
         {
-            Button btn = new Button(this);
-            btn.setText((String)movies[i]);
-            layout.addView(btn);
+            for (String movie_name : list_of_movies) {
+                Button btn = new Button(this);
+                btn.setText(movie_name);
+                layout.addView(btn);
+            }
         }
-    }
-
-    /*
-        On action when user searches for a movie
-        Parameters: None
-        Return: None
-        TODO: Function also has to be bound to UI element
-    */
-    public void searchMovie(View v)
-    {
-        SearchView search_box = (SearchView)findViewById(R.id.movies_search_box);
-        String search_criteria = search_box.getQuery().toString().trim();
-
-        Log.println(Log.INFO, "tag", "Searching with criteria: " + search_criteria);
-
-        //TODO: Based on search criteria, create an array of movie objects. SEARCH_CRITERIA MUST BE SUBSTRING OF MOVIE_NAME
-        Object[] movies = movie_names;
-
-        //Once we know what movies meet search criteria, populate them in-app
-        populateMovies(movies);
-    }
-
-    /*
-        On action for filtering currently running and upcoming movies
-        TODO: Function also has to be bound to UI element
-    */
-    public void filterMovie(View v)
-    {
-        Spinner spinner = (Spinner)findViewById(R.id.movies_filter);
-
-        String filter_criteria = spinner.getSelectedItem().toString();
-        String[] array = getResources().getStringArray(R.array.movie_display_filters);
-
-        if (Arrays.asList(array).contains(filter_criteria))
-        { //ensure valid criteria
-            Log.println(Log.INFO, "tag", "Filtering with criteria: " + filter_criteria);
-
-            //TODO: Based on filter criteria, create an array of movie objects.
-            Object[] movies = movie_names;
-
-            //Once we know what movies meet filter criteria, populate them in-app
-            populateMovies(movies);
+        else
+        {
+            //print error message saying list is null (not empty, but null)
         }
     }
 }
