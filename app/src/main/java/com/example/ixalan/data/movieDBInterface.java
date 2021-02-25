3d9bc3d5c2459f1c0a6c8e9c88beeb95aa93b72b
Mario Quijada-Rodriguez <umquijam@myumanitoba.ca>
2021-02-25 02:05:15 -0600

    Interfaces of fakeDB's

---
+++ app/src/main/java/com/example/ixalan/data/movieDBInterface.java
@@ -0,0 +1,10 @@
+public  interface movieDBInterface{
+
+    // Get a User from the database by there movieID
+    public Movie getMovie(int movieID);
+
+    //removes a movie completely from the dataBase
+    // updating should not use this function
+    public void removeMovie(int movieID);
+
+}
