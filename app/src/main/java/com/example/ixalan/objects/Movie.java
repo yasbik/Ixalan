54b995b455e66a6ac88e3c4422ab8ff81f682185
Ramanpreet Kaur <rk1@myumanitoba.ca>
2021-02-26 06:52:27 -0600

    added 8 unit tests

--- app/src/main/java/com/example/ixalan/objects/Movie.java
+++ app/src/main/java/com/example/ixalan/objects/Movie.java
@@ -11,7 +11,7 @@ public class Movie
     String posterUrl;// Contains a url as a string for the poster for the movie. 
     String releaseDate;// The date the movie was released. 
     String synopsis;// A brief summary of the movie.
-    String trailerUrl;// Contains a url to tge trailer of the movie.
+    String trailerUrl;// Contains a url to the trailer of the movie.
     ArrayList<Merchandise> merchandises = new ArrayList<>();// A list of merchandises of the movie implemented by array list but can be made an array
     //We can also have a 2d array of the theater and the time the movie is playing 
     ArrayList<Theatre> theatres = new ArrayList<>();// List of theatre the movie is being payed in
