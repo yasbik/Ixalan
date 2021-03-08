0c1daa8de2eab77bdc387eceaa64df90d1f70538
Mrinalpm <mrinalpm@hotmail.com>
2021-03-08 15:33:26 -0600

    Added getters to objects

--- app/src/main/java/com/example/ixalan/objects/Merchandise.java
+++ app/src/main/java/com/example/ixalan/objects/Merchandise.java
@@ -2,9 +2,34 @@ package com.example.ixalan.objects;
 
 public class Merchandise
 {
-    String name;
-    String description;
-    Movie movieTag;
-    float price;
-    int stock;
+    private String name;
+    private String description;
+    private Movie movieTag; //movie to which this merchandise item is associated
+    private float price;
+    private int stock;
+
+    public String getName()
+    {
+        return this.name;
+    }
+
+    public String getDescription()
+    {
+        return this.description;
+    }
+
+    public Movie getMovieTag()
+    {
+        return this.movieTag;
+    }
+
+    public float getPrice()
+    {
+        return this.price;
+    }
+
+    public int getStock()
+    {
+        return this.stock;
+    }
 }
--- app/src/main/java/com/example/ixalan/objects/Movie.java
+++ app/src/main/java/com/example/ixalan/objects/Movie.java
@@ -1,25 +1,59 @@
 package com.example.ixalan.objects;
 
 //A movie class for our Theatre booking app.
+import java.time.LocalDate;
 import java.util.ArrayList;
 public class Movie
 {
-    int movieID; // primary key for movies
-    String Name;// The name of the movie is stored in string
-    float rating;// The rating of the movie(maybe out of 5 ) is stored as float to handle numbers like 3.8
-    ArrayList<String> castCrew = new ArrayList<>();// A list of cast members involved in the making of the movie implemented by array list but can be made an array
-    String posterUrl;// Contains a url as a string for the poster for the movie. 
-    String releaseDate;// The date the movie was released. 
-    String synopsis;// A brief summary of the movie.
-    String trailerUrl;// Contains a url to the trailer of the movie.
-    ArrayList<Merchandise> merchandises = new ArrayList<>();// A list of merchandises of the movie implemented by array list but can be made an array
+    private int movieID; // primary key for movies (Possibly alphanumeric)
+    private String Name;// The name of the movie is stored in string
+    private float rating;// The rating of the movie(maybe out of 5 ) is stored as float to handle numbers like 3.8
+    private ArrayList<String> castCrew = new ArrayList<>();// A list of cast members involved in the making of the movie implemented by array list but can be made an array
+    private String posterUrl;// Contains a url as a string for the poster for the movie.
+    private LocalDate releaseDate;// The date the movie was released.
+    private  String synopsis;// A brief summary of the movie.
+    private String trailerUrl;// Contains a url to the trailer of the movie.
+    private ArrayList<Merchandise> merchandises = new ArrayList<>();// A list of merchandises of the movie implemented by array list but can be made an array
     //We can also have a 2d array of the theater and the time the movie is playing 
-    ArrayList<Theatre> theatres = new ArrayList<>();// List of theatre the movie is being payed in
+    private ArrayList<Theatre> theatres = new ArrayList<>();// List of theatre the movie is being payed in
 
     public Movie(String name, String posterUrl)
     {
         this.Name = name;
         this.posterUrl = posterUrl;
+    }
+
+    public ArrayList<Theatre> getTheatres() {
+        return this.theatres;
+    }
+
+    public ArrayList<Merchandise> getMerchandises() {
+        return this.merchandises;
+    }
+
+    public String getSynopsis()
+    {
+        return this.synopsis;
+    }
+
+    public String getTrailerUrl()
+    {
+        return this.trailerUrl;
+    }
+
+    public LocalDate getReleaseDate()
+    {
+        return this.releaseDate;
+    }
+
+    public float getRating()
+    {
+        return this.rating;
+    }
+
+    public ArrayList<String> getCastCrew()
+    {
+        return this.castCrew;
     }
 
     public String getMovieName()
