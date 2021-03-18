5390546ada2d3a273b2bc4340f0e8146703c1346
Mrinalpm <mrinalpm@hotmail.com>
2021-03-18 10:08:47 -0500

    Trailer + movie details feature added

--- app/src/main/java/com/example/ixalan/presentation/MovieDetailActivity.java
+++ app/src/main/java/com/example/ixalan/presentation/MovieDetailActivity.java
@@ -2,9 +2,13 @@ package com.example.ixalan.presentation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import com.example.ixalan.objects.Movie;
@@ -33,6 +37,14 @@ public class MovieDetailActivity extends AppCompatActivity {
             ImageView movie_poster_imageview = (ImageView) findViewById(R.id.movie_poster_imageview);
             int movie_poster_id = getResources().getIdentifier(movie_poster_file_name, "drawable", getPackageName());
             movie_poster_imageview.setImageResource(movie_poster_id);
+            movie_poster_imageview.setClickable(true);
+            movie_poster_imageview.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v)
+                {
+                    watchYoutubeVideo(movie.getTrailerUrl());
+                }
+            });
 
             //Display movie name
             TextView movie_name_textview = (TextView) findViewById(R.id.movie_name_textview);
@@ -44,5 +56,22 @@ public class MovieDetailActivity extends AppCompatActivity {
             movie_details_textview.setText(movie_synopsis);
         }
 
+    }
+
+    public void watchYoutubeVideo(String id)
+    {
+        String url = "http://www.youtube.com/watch?v="+id;
+        try {
+            Intent i = new Intent("android.intent.action.MAIN");
+            i.setComponent(ComponentName.unflattenFromString("com.android.chrome/com.android.chrome.Main"));
+            i.addCategory("android.intent.category.LAUNCHER");
+            i.setData(Uri.parse(url));
+            startActivity(i);
+        }
+        catch(ActivityNotFoundException e) {
+            // Chrome is not installed
+            Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(i);
+        }
     }
 }
