c38776673cb7587b22ea8653eca0952e31f21ae1
Ramanpreet Kaur <rk1@myumanitoba.ca>
2021-04-08 06:10:12 -0500

    payment feature code smells eliminated

--- app/src/main/java/ixalan/movieapp/presentation/CardPaymentActivity.java
+++ app/src/main/java/ixalan/movieapp/presentation/CardPaymentActivity.java
@@ -2,9 +2,11 @@ package ixalan.movieapp.presentation;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -45,11 +47,51 @@ public class CardPaymentActivity extends AppCompatActivity {
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(CardPaymentActivity.this, PaymentSuccessfulActivity.class);
-                intent.putExtra("PAYMENT_MODE", "card");
-                startActivity(intent);
+
+                EditText card_number_text = (EditText) findViewById(R.id.card_number_text);
+                EditText expiry_date_text = (EditText) findViewById(R.id.expiry_date_text);
+                EditText security_code_text = (EditText) findViewById(R.id.security_code_text);
+                EditText card_name_text = (EditText) findViewById(R.id.card_name_text);
+                EditText postal_code_text = (EditText) findViewById(R.id.postal_code_text);
+
+                String card_number = card_number_text.getText().toString().trim();
+                String expiry_date = expiry_date_text.getText().toString().trim();
+                String security_code = security_code_text.getText().toString().trim();
+                String card_name = card_name_text.getText().toString().trim();
+                String postal_code = postal_code_text.getText().toString().trim();
+
+                if (TextUtils.isEmpty(card_number) || card_number.length() != 16) {
+                    card_number_text.setError("Please Enter Valid Credit Card number!");
+
+                } else if (TextUtils.isEmpty(expiry_date) || expiry_date.length() != 5 || expiry_date.charAt(2) != '/' ||
+                        expiry_date.charAt(0) > '1' || (expiry_date.charAt(0) == '1' && expiry_date.charAt(1) > '2') ) {
+                    expiry_date_text.setError("Please Enter Valid Credit Card expiry date!");
+
+                } else if (TextUtils.isEmpty(security_code) || security_code.length() != 3) {
+                    security_code_text.setError("Please Enter Valid Credit Card CVV!");
+
+                } else if (TextUtils.isEmpty(card_name) || alphabetic(card_name) == false) {
+                    card_name_text.setError("Please Enter Valid Name!");
+
+                } else if (TextUtils.isEmpty(postal_code)) {
+                    postal_code_text.setError("Please Enter Valid Postal Code!");
+
+                } else {
+                    Intent intent = new Intent(CardPaymentActivity.this, PaymentSuccessfulActivity.class);
+                    startActivity(intent);
+                }
             }
         });
+    }
+
+    private boolean alphabetic(String str) {
+        boolean is_alphabetic = true;
+        for (int i=0; i< str.length(); i++) {
+            if (!Character.isLetter(str.charAt(i)) && str.charAt(i) != ' ') {
+                is_alphabetic = false;
+            }
+        }
+        return is_alphabetic;
     }
 }
 
