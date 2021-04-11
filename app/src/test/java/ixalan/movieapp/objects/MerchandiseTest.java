adcbed9a3dd2cb5a823f65c5ac6289ed5065902c
Mrinalpm <mrinalpm@hotmail.com>
2021-04-11 00:02:33 -0500

    Implemented MerchandiseTest

--- app/src/test/java/ixalan/movieapp/objects/MerchandiseTest.java
+++ app/src/test/java/ixalan/movieapp/objects/MerchandiseTest.java
@@ -1,6 +1,36 @@
 package ixalan.movieapp.objects;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class MerchandiseTest
 {
     //TODO: Write tests
+
+    @Test
+    public void test1()
+    {
+        Merchandise merchandise;
+
+        System.out.println("\nStarting MerchandiseTest\n");
+
+        merchandise = new Merchandise(1, "Test Merchandise", (float)2.99);
+        merchandise.setDescription("Test Description");
+        merchandise.setMovieTag(1000);
+        merchandise.setStock(15);
+        merchandise.setImageUrl("Test Image URL");
+
+        assertNotNull(merchandise);
+        assertEquals(merchandise.getMerchID(), 1);
+        assert(merchandise.getPrice() == 2.99);
+        assertTrue("Test Merchandise".equals(merchandise.getName()));
+        assertTrue("Test Description".equals(merchandise.getDescription()));
+        assertEquals(1000, merchandise.getMovieTag());
+        assertEquals(15, merchandise.getStock());
+        assertTrue("Test Image URL".equals(merchandise.getImageUrl()));
+        assertEquals(1,merchandise.getQuantity());
+
+        System.out.println("\nFinished MerchandiseTest\n");
+    }
 }
