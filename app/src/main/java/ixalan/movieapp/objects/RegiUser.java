600f94735915105ff533543a2dbaddbae939eb94
Mrinalpm <mrinalpm@hotmail.com>
2021-04-14 20:17:01 -0500

    Reverted changes

--- app/src/main/java/ixalan/movieapp/objects/RegiUser.java
+++ app/src/main/java/ixalan/movieapp/objects/RegiUser.java
@@ -3,11 +3,19 @@ package ixalan.movieapp.objects;
 import java.util.ArrayList;
 public class RegiUser extends User {
     private String id; //Possibly alphanumeric
-    private char[] password; //Store password as char array for mroe security
-    private ArrayList<Movie> moviesWatched = new ArrayList<>();
-    private ArrayList<Movie> moviesLiked = new ArrayList<>();
+    private char[] password; //Store password as char array for more security
+    private ArrayList<Movie> moviesWatched ;
+    private ArrayList<Movie> moviesLiked ;
     private String paymentInfo;
     private String savedAddress;
+
+    public RegiUser(String id , char[] password , String savedAddress){
+        this.id = id;
+        this.password = password;
+        moviesWatched = new ArrayList<>();
+        moviesLiked = new ArrayList<>();
+        this.savedAddress = savedAddress;
+    }
 
     public String getId()
     {
